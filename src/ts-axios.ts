3b948e04daf460c7ed94811961094a5da0a6a89d
localsummer <roamingcode@163.com>
2019-09-18 00:19:24 +0800

    feat: add xhr request

--- src/ts-axios.ts
+++ src/ts-axios.ts
@@ -1,4 +1,11 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export default class DummyClass {}
+import { AxiosRequestConfig } from './types'
+import xhr from './xhr'
+
+function axios(config: AxiosRequestConfig) {
+  xhr(config)
+}
+
+export default axios
